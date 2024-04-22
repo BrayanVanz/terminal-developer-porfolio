aac4677e93bf08a18ef174ad150773b4cff3fc62
Brayan Vanz de Oliveira <brayanvanzdeoliveira@gmail.com>
2024-04-21 21:53:06 -0300

    Finish showHelp() function
    Complete functionalities attached to the help command.

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
+const body = document.querySelector("body");
 const shell = document.querySelector("input");
+const header = document.querySelector("header");
+const userInput = document.querySelector("#userInput");
 
 shell.addEventListener("keydown", (press) => {
     if (press.key == "Enter") {
@@ -13,9 +16,33 @@ function chooseCMD(cmd) {
     switch (cmd) {
         case "help":
             showHelp();
+            break;
     }
 }
 
 function showHelp() {
-    console.log("Here is a list of all commands")
+    const options = {
+        whois : "- Learn more about me.",
+        experience : "- Work related experience.",
+        projects : " - Personal projects I have worked on.",
+        skills : "- Tools I have studied and used previously.",
+        education : "- Formal education and certifications.",
+        contact : "- Channels for professional inquiries."
+    };
+
+    const commandSection = document.createElement("div");
+
+    Object.keys(options).forEach( (option) => {
+        const command = document.createElement("h4");
+        command.textContent = option;
+        
+        const description = document.createElement("p");
+        description.textContent = options[option];
+
+        commandSection.appendChild(command);
+        commandSection.appendChild(description);
+    });
+
+    body.insertBefore(commandSection, userInput);
+    shell.value = '';
 }
